875a4f650e87ce48797d4b9679f5345cac30042c
Aman Singh <amansinghx04@gmail.com>
2026-02-19 14:17:08 +0530

    Updated

--- NodeJS-1/index4a.js
+++ NodeJS-1/index4a.js
@@ -1,20 +1,43 @@
-import express, { request, response } from "express";
-const app = express();
-app.listen(8080, () => {
-    console.log("Server Started....")
-})
+import express from "express"
+const app= express();
+app.listen(8070,()=>{
+    console.log("Server Started");
+});
+app.get("/:id/:id1/:id2/:id3", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    res.send("hello students");
+});
 
-app.get("/:num1/:num2", (request,response) => {
-    const num1 = Number(request.params.num1);
-    const num2 = Number(request.params.num2);
+app.get("/:id/:id1", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    const id = Number(req.params.id);
+    const id1 = Number(req.params.id1);
+    const sum = id + id1;
+    res.send(`Sum is: ${sum}`);
+});
 
-    response.send((num1 + num2).toString());
-})
 
-app.get("/x/:num1/y/:num2/z/:num3", (request,response) => {
-    const num1 = Number(request.params.num1);
-    const num2 = Number(request.params.num2);
-    const num3 = Number(request.params.num2);
+app.get("/x/y", (req, res) => {
+    res.send("Good morning");
+});
 
-    response.send((num1 + num2 + num3).toString());
-})
+
+
+
+x/4/y/5/z/4
+import express from "express"
+const app= express();
+app.listen(8070,()=>{
+    console.log("Server Started");
+});
+app.get("/x/:id/y/:id1/z/:id2", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    const id = Number(req.params.id);
+    const id1 = Number(req.params.id1);
+    const id2 = Number(req.params.id2);
+    const sum = id + id1 + id2;
+    res.send(`Sum is: ${sum}`);
+});
