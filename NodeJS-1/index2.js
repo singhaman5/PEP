059eced9578f814d9bc9265c74156aa0725ccb52
Aman Singh <amansinghx04@gmail.com>
2026-02-19 11:30:11 +0530

    Updated

---
+++ NodeJS-1/index2.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express()
+app.listen(8080, () => {
+    console.log("Server started");
+});
+app.get("/",(req,res)=>{
+    console.log(req.url)
+    res.send("Response from server for /")
+})
