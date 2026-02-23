ab11dcefee0c1ef716d90099e9a73d910b9550d4
Aman Singh <amansinghx04@gmail.com>
2026-02-23 13:43:30 +0530

    Updated

---
+++ NodeJS-1/index14.js
@@ -0,0 +1,13 @@
+import express from "express"
+import jwt from "jsonwebtoken"
+const SECRET = "lpu"
+const app = express()
+app.listen(8080)
+const user = {
+    name: "John",
+    email: "john@gmail.com",
+    role: "student"
+}
+
+const token = jwt.sign(user,SECRET,{expiresIn: "1h"})
+console.log(token)
