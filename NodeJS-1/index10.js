4c8798a899181f63ee26617bd2a787b435cbe892
Aman Singh <amansinghx04@gmail.com>
2026-02-23 09:37:39 +0530

    Updated

---
+++ NodeJS-1/index10.js
@@ -0,0 +1,6 @@
+import express from "express"
+const app = express()
+app.listen(8080, () => console.log("Server started"))
+app.get("/", (req,res) => {
+    res.status(201).json({message: "Hello World"});
+})
